422c51a4e65bcedd10f5ba4e9d57919eea84bbc7
douglas <douglascardoso@usp.br>
2021-05-08 02:22:36 -0300

    add .gitignore

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
-CHROME_PROFILE_PATH = "--user-data-dir=/home/douglas/.config/google-chrome/Wtsp"
+# this code is for Linux Ubuntu 
+CHROME_PROFILE_PATH = "--user-data-dir=/home/<username>/.config/google-chrome/Wtsp"
