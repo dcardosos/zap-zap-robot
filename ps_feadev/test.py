a621ac5cfe58ba9fccf2fbe5673906f022b0c632
douglas <douglascardoso@usp.br>
2021-05-14 02:55:22 -0300

    zapzap robot to private message

--- ps_feadev/test.py
+++ ps_feadev/test.py
@@ -11,8 +11,8 @@ import sys
 from config_ import CHROME_PROFILE_PATH
 
 # open codes
-with open('numbers.txt', 'r', encoding='utf8') as f:
-            numbers = [num.strip() for num in f.readlines()]
+with open('codes.txt', 'r', encoding='utf8') as f:
+            codes = [code.strip() for code in f.readlines()]
 
 # open messages
 with open('message.txt', 'r', encoding='utf8') as f:
@@ -21,44 +21,29 @@ with open('message.txt', 'r', encoding='utf8') as f:
 # instanciate option
 options = Options()
 options.add_argument(CHROME_PROFILE_PATH)
- 
-# creating a browser with options
-browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
-
-# open and maximize window
-browser.maximize_window()
 
 # read each code in codes.txt
-for n in numbers:
-    # go to link to send message
-    msg = msg.replace(' ', '%20')
-    browser.get(f'https://api.whatsapp.com/send/?phone={n}&text={msg}&app_absent=0')
-    time.sleep(2)
+for c in codes:
+    
+    # creating a browser with options
+    browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+
+    # open and maximize window
+    browser.maximize_window()
+
+    # come in group link
+    browser.get(f'https://api.whatsapp.com/send/?phone=55{number}&text&app_absent=0')
+    time.sleep(10)
 
     # click in button "continue to chat"
-    continue_chat = '//*[@id="action-button"]' 
-    get_c_chat = browser.find_element_by_xpath(continue_chat)
-    get_c_chat.click()
+    join =  '//*[@id="action-button"]'  
+    get_join = browser.find_element_by_xpath(join)
+    get_join.click()
                 
     time.sleep(5)
-
-    # click in button 'use whatsapp web'
-    use_wpp = '//*[@id="fallback_block"]/div/div/a'
-    get_use_wpp = browser.find_element_by_xpath(use_wpp) 
-    get_use_wpp.click()
-
-    time.sleep(3)
-
-    # click in input text place in chat
-    input_xpath = '//div[@contenteditable="true"][@data-tab="6"]'
-    input_box = browser.find_element_by_xpath(input_xpath)
+   
+    # click wpp web
     
-    # send the message
-    input_box.send_keys(Keys.ENTER)
-    time.sleep(3)
-    
-
-'''    
     # click in input text place in chat            
     input_xpath = '//div[@contenteditable="true"][@data-tab="6"]'
     input_box = browser.find_element_by_xpath(input_xpath)
@@ -66,10 +51,12 @@ for n in numbers:
 
     # write the message
     pyperclip.copy(msg)
-
+   
     # send the message  
     input_box.send_keys(Keys.SHIFT, Keys.INSERT)  # Keys.CONTROL + "v"'   
-
+    input_box.send_keys(Keys.ENTER)
+    time.sleep(1)
+    
     # click in menu
     menu_xpath = '//*[@id="main"]/header/div[3]/div/div[2]/div/div'
     button_menu = browser.find_element_by_xpath(menu_xpath)
@@ -93,5 +80,3 @@ for n in numbers:
 
     # close the window
     browser.close()
-
-'''
